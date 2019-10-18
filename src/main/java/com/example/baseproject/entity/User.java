ddea6d75ae228bc8b690f3fe74b673c0a3b8d6e3
yuyang <502950715@qq.com>
2019-10-18 17:00:20 +0800

    使用lombok

--- src/main/java/com/example/baseproject/entity/User.java
+++ src/main/java/com/example/baseproject/entity/User.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * 用户model
@@ -14,6 +12,7 @@ import lombok.Setter;
  * @since 1.0
  * Create with Intellij IDEA on 2019-10-17 11:56
  */
+@Data
 @TableName("user")
 public class User {
 
@@ -32,43 +31,4 @@ public class User {
     @TableField("token")
     private String token;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
